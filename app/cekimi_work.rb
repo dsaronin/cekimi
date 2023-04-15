c3bccf5785a6b39dd7c2c8c8f5f1d44c1a1cd95e
dsaronin <dsaronin@gmail.com>
2023-04-14 21:08:10 -0700

    starting command loop skeleton

--- app/cekimi_work.rb
+++ app/cekimi_work.rb
@@ -21,14 +21,57 @@ class CekimiWork
 
   def setup_work()
     Environ.log_info( "starting..." )
+    Environ.put_prompt "\tçekimi: merhaba dunia!\n"
   end
 
   def do_work()
-    Environ.put_prompt "\n\tçekimi: merhaba dunia!\n\n"
+      # command prompt
+    Environ.put_prompt("${getPrompt()} > ")  until 
+      parse_commands( Environ.get_command_line )
+ 
   end
 
   def shutdown_work()
     Environ.log_info( "...ending" )
   end
 
-end
+  # parse_commands -- parses the command string and executes commands
+  # args:
+  #   cmdlist: array of syntatical elements
+  # returns boolean: true if continue looping
+  def parse_commands( cmdlist )        
+        loop = true                 # user input loop while true
+        dropCount = 1               # assume need to drop cmd from list head
+
+        # parse command
+        when ( cmd = cmdlist.first().trim() ) {
+            # list rules
+            "list"  -> 
+            "l"     -> 
+
+
+            # status
+            "sts", "status" -> 
+            "s"   -> 
+
+            "f", "flags"     -> 
+            "h", "help"      -> 
+            "v", "version"   -> 
+            "o", "options"   -> 
+
+            "x", "ex", "exit"       -> loop = false   # exit program
+            "q", "quit"             -> loop = false  # exit program
+
+            ""               -> loop = true   # empty line; NOP
+            else        ->   # treat as cekimi conjugate request
+        }
+
+        # useKamusi will be non-null if a search command was encountered
+        # searchKeyList( cmdlist.drop(dropCount) )   # conjugate
+
+        return loop
+                
+  end
+
+
+end  # class CekimiWork
